1e71c199b22fcd8b17c2cad7d34a9060c5902ad5
Saqib Masood <masoodsaqib00@gmail>
2024-04-21 17:00:03 +0530

    7th suite of  Test case also passed

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ async function executeSELECTQuery(query) {
     const filteredData = whereClauses.length > 0
         ? data.filter(row => whereClauses.every(clause => {
             // You can expand this to handle different operators
-            return row[clause.field] === clause.value;
+            return evaluateCondition(row,clause);
         }))
         : data;
 
@@ -23,5 +23,16 @@ async function executeSELECTQuery(query) {
         return selectedRow;
     });
 }
-
+function evaluateCondition(row, clause) {
+    const { field, operator, value } = clause;
+    switch (operator) {
+        case '=': return row[field] === value;
+        case '!=': return row[field] !== value;
+        case '>': return row[field] > value;
+        case '<': return row[field] < value;
+        case '>=': return row[field] >= value;
+        case '<=': return row[field] <= value;
+        default: throw new Error(`Unsupported operator: ${operator}`);
+    }
+}
 module.exports = executeSELECTQuery;
